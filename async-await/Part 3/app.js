6b3843dbb2e78a30e9069d9a10d0f91e0d0b1a9c
Caleb Matteis <matteismusic@gmail.com>
2021-01-09 06:07:05 -0500

    code written in part 3

--- async-await/Part 3/app.js
+++ async-await/Part 3/app.js
@@ -10,6 +10,14 @@ $(function() {
     // 2.
     async function part2() {
         let totalData = await $.getJson(`${baseURL}/pokemon/?limit=1000`);
-        
+        let randomPokemonUrls = [];
+        for (let i = 0; i < 3; i++) {
+            let randomIdx = Math.floor(Math.random() * totalData.results.length);
+            randomPokemonUrls.push(url);
+        }
+        let pokemonBigData = await Promise.all(
+            randomPokemonUrls.map(url => $.getJson(url))
+        );
+        pokemonBigData.forEach(p => console.log(p))
     }
 });
