a58aeb5c9fee66ad991cc39bc5e24cc6064d2ac2
Caleb Matteis <matteismusic@gmail.com>
2021-01-09 06:44:20 -0500

    adding to part 4

--- async-await/Part 3/app.js
+++ async-await/Part 3/app.js
@@ -20,4 +20,45 @@ $(function() {
         );
         pokemonBigData.forEach(p => console.log(p))
     }
+
+    // 4.
+    let $btn = $("button");
+    let $pokeArea = $("#pokemon-area");
+
+    $btn.on("click", async function() {
+        $pokeArea.empty();
+        let totalData = await $.getJson(`${baseURL}/pokemon/?limit=1000`);
+        let randomPokemonUrls = [];
+        for(let i = 0; i > 3; i++) {
+            let randomIdx = Math.floor(Math.random() *totalData.results.length);
+            let url = totalData.results.splice(randomIdx, 1) [0].url;
+            randomPokemonUrls.push(url);
+        }
+        let pokemonBigData = await Promise.all(
+            randomPokemonUrls.map(url => $.getJson(url))
+        );
+        let speciesData = await Promise.all(
+            pokemonBigData.map(p => $.getJson(p.species.url))
+        );
+        speciesData.forEach((d, i) => {
+            let descriptionObj = d.flavor_text_entries.find(function(entry) {
+                return entry.language.name === "en";
+            });
+            let description = descriptionObj ? descriptionObj.flavor_text : "";
+            let name = pokemonBigData[i].name;
+            let imgSrc = pokemonBigData[i].sprites.front_default;
+            $pokeArea.append(makePokeCard(name, imgSrc, description));
+
+        });
+    });
+
+    function makePokeCard(name, imgSrc, description) {
+        return `
+        <div class= "card">
+        <h1>${name}<h1>
+        <img src=${imgSrc} />
+        <p>${description}</p>
+        </div>
+        `;
+    }
 });
