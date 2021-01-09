fdea25952ae215c50b40e4cb06696e6269c42ea5
Caleb Matteis <matteismusic@gmail.com>
2021-01-09 06:53:33 -0500

    fixed javascript so cards would appear

--- async-await/Part 3/app.js
+++ async-await/Part 3/app.js
@@ -24,41 +24,41 @@ $(function() {
     // 4.
     let $btn = $("button");
     let $pokeArea = $("#pokemon-area");
-
+  
     $btn.on("click", async function() {
-        $pokeArea.empty();
-        let totalData = await $.getJson(`${baseURL}/pokemon/?limit=1000`);
-        let randomPokemonUrls = [];
-        for(let i = 0; i > 3; i++) {
-            let randomIdx = Math.floor(Math.random() *totalData.results.length);
-            let url = totalData.results.splice(randomIdx, 1) [0].url;
-            randomPokemonUrls.push(url);
-        }
-        let pokemonBigData = await Promise.all(
-            randomPokemonUrls.map(url => $.getJson(url))
-        );
-        let speciesData = await Promise.all(
-            pokemonBigData.map(p => $.getJson(p.species.url))
-        );
-        speciesData.forEach((d, i) => {
-            let descriptionObj = d.flavor_text_entries.find(function(entry) {
-                return entry.language.name === "en";
-            });
-            let description = descriptionObj ? descriptionObj.flavor_text : "";
-            let name = pokemonBigData[i].name;
-            let imgSrc = pokemonBigData[i].sprites.front_default;
-            $pokeArea.append(makePokeCard(name, imgSrc, description));
-
+      $pokeArea.empty();
+      let allData = await $.getJSON(`${baseURL}/pokemon/?limit=1000`);
+      let randomPokemonUrls = [];
+      for (let i = 0; i < 3; i++) {
+        let randomIdx = Math.floor(Math.random() * allData.results.length);
+        let url = allData.results.splice(randomIdx, 1)[0].url;
+        randomPokemonUrls.push(url);
+      }
+      let pokemonData = await Promise.all(
+        randomPokemonUrls.map(url => $.getJSON(url))
+      );
+      let speciesData = await Promise.all(
+        pokemonData.map(p => $.getJSON(p.species.url))
+      );
+      speciesData.forEach((d, i) => {
+        let descriptionObj = d.flavor_text_entries.find(function(entry) {
+          return entry.language.name === "en";
         });
+        let description = descriptionObj ? descriptionObj.flavor_text : "";
+        let name = pokemonData[i].name;
+        let imgSrc = pokemonData[i].sprites.front_default;
+        $pokeArea.append(makePokeCard(name, imgSrc, description));
+      });
     });
-
+  
     function makePokeCard(name, imgSrc, description) {
-        return `
-        <div class= "card">
-        <h1>${name}<h1>
-        <img src=${imgSrc} />
-        <p>${description}</p>
+      return `
+        <div class="card">
+          <h1>${name}</h1>
+          <img src=${imgSrc} />
+          <p>${description}</p>
         </div>
-        `;
+      `;
     }
-});
+  });
+  
