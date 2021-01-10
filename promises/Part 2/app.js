88e6302a35aaf58f3f50e31c4bd09f79bb3c8c2b
Caleb Matteis <matteismusic@gmail.com>
2021-01-09 19:49:28 -0500

    added part 3 to part 2 of promises

--- promises/Part 2/app.js
+++ promises/Part 2/app.js
@@ -26,4 +26,30 @@ $(function() {
     });
 
     // 3.
+    let deckId = null;
+    let $btn = $('button');
+    let $cardArea = $('#card-area');
+
+    $.getJSON(`${baseURL}/new/shuffle/`).then(data => {
+        deckId = data_id;
+        $btn.show();
+    });
+
+    $btn.on('click', function() {
+        $.getJSON(`${baseURL}/${deckId}/draw/`).then(data => {
+            let cardSrc = data.cards[0].image;
+            let angle = Math.random() * 90 - 45;
+            let randomX = Math.random() * 40 - 20;
+            let randomY = Math.random() * 40 - 20;
+            $cardArea.append(
+                $('<img>', {
+                    src: cardSrc,
+                    css: {
+                        transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+                    }
+                })
+            );
+            if (data.remaining === 0) $btn.remove();
+        });
+    })
 });
