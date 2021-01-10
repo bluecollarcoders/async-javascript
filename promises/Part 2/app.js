2af98956da8f6c5a26a2386178c442163171a11f
Caleb Matteis <matteismusic@gmail.com>
2021-01-09 20:00:09 -0500

    added css style to part 2 of part 2 promises

--- promises/Part 2/app.js
+++ promises/Part 2/app.js
@@ -27,29 +27,29 @@ $(function() {
 
     // 3.
     let deckId = null;
-    let $btn = $('button');
-    let $cardArea = $('#card-area');
+  let $btn = $('button');
+  let $cardArea = $('#card-area');
 
-    $.getJSON(`${baseURL}/new/shuffle/`).then(data => {
-        deckId = data_id;
-        $btn.show();
+  $.getJSON(`${baseURL}/new/shuffle/`).then(data => {
+    deckId = data.deck_id;
+    $btn.show();
+  });
+
+  $btn.on('click', function() {
+    $.getJSON(`${baseURL}/${deckId}/draw/`).then(data => {
+      let cardSrc = data.cards[0].image;
+      let angle = Math.random() * 90 - 45;
+      let randomX = Math.random() * 40 - 20;
+      let randomY = Math.random() * 40 - 20;
+      $cardArea.append(
+        $('<img>', {
+          src: cardSrc,
+          css: {
+            transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+          }
+        })
+      );
+      if (data.remaining === 0) $btn.remove();
     });
-
-    $btn.on('click', function() {
-        $.getJSON(`${baseURL}/${deckId}/draw/`).then(data => {
-            let cardSrc = data.cards[0].image;
-            let angle = Math.random() * 90 - 45;
-            let randomX = Math.random() * 40 - 20;
-            let randomY = Math.random() * 40 - 20;
-            $cardArea.append(
-                $('<img>', {
-                    src: cardSrc,
-                    css: {
-                        transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
-                    }
-                })
-            );
-            if (data.remaining === 0) $btn.remove();
-        });
-    })
+  });
 });
